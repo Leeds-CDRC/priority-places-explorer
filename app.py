cedf5a90704e88969d32c123f53fff1b58f10f44
peterbaudains <98837192+peterbaudains@users.noreply.github.com>
2022-09-08 13:45:57 +0100

    Update data, basemap and colormap

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ app.layout = html.Div(style={
     Output("graph", "figure"), 
     Input("domain", "value"))
 def display_choropleth(domain):
-    df = pd.read_csv('/data/priority_places_v0_2_decile_domains_WGS.csv',
+    df = pd.read_csv('/data/priority_places_v0_3_decile_domains_WGS.csv',
                      dtype={'domain_supermarket_proximity':'category',
                             'domain_supermarket_transport':'category',
                             'domain_ecommerce_access':'category',
@@ -52,22 +52,36 @@ def display_choropleth(domain):
                             'domain_fuel_poverty':'category', 
                             'combined': 'category'}
     )
-    colormap = ['#fff7f3', '#fde3df', '#fccbc6', '#fbacb9', '#f880aa', '#eb519d', '#cd238f', '#a2017c', '#750176', '#49006a']
+    colormap = ['#0d0887',
+                '#41049d',
+                '#6a00a8',
+                '#8f0da4',
+                '#b12a90',
+                '#cc4778',
+                '#e16462',
+                '#f2844b',
+                '#fca636',
+                '#fcce25']
     colormap.reverse()
+
     fig = px.scatter_mapbox(
                         df, 
                         lat='latitude',
                         lon='longitude', 
                         color=domain, 
-                        hover_name=domain, 
-                        hover_data={'latitude': False, 
-                                    'longitude': False, 
-                                    'geo_code': False, 
-                                    domain: False},
                         color_discrete_sequence=colormap,
+                        custom_data=['geo_code', 
+                                     'domain_supermarket_proximity',
+                                     'domain_supermarket_transport',
+                                     'domain_ecommerce_access',
+                                     'domain_socio_demographic',
+                                     'domain_nonsupermarket_proximity',
+                                     'domain_food_for_families',
+                                     'domain_fuel_poverty',
+                                     'combined'],
                         center={'lat': 53.8067, 'lon': -1.5550}, 
                         category_orders={domain: ['1', '2', '3', '4', '5', '6', '7','8', '9', '10']})
-    fig.update_layout(mapbox_style='open-street-map')
+    fig.update_layout(mapbox_style='carto-positron')
     fig.update_layout(margin={'r':0, 't':0, 'l':0, 'b':0})
     fig.update_layout(legend=dict(
         yanchor="top",
@@ -78,7 +92,16 @@ def display_choropleth(domain):
     ))
     fig.update_layout(legend_title_text='Decile (1 = highest priority)')
     
-    #fig.update_traces(hovertemplate="") #
+    fig.update_traces(hovertemplate=(
+                        '<b>Geo Code</b>: %{customdata[0]}<br>'+\
+                        'Proximity to supermarket food retail facilities decile: %{customdata[1]}<br>'+\
+                        'Accessibility to supermarket retail facilties decile: %{customdata[2]}<br>'+\
+                        'Access to online deliveries decile: %{customdata[3]}<br>'+\
+                        'Socio-demographic barriers decile: %{customdata[4]}<br>'+\
+                        'Proximity to non-supermarket food retail facilities decile: %{customdata[5]}<br>'+\
+                        'Food support for families decile: %{customdata[6]}<br>'+\
+                        'Fuel poverty decile: %{customdata[7]}<br>'
+                        'Combined Priority Places Index decile: %{customdata[8]}<br>'))
     fig.update_geos(fitbounds="locations", visible=True)
     return fig
 
